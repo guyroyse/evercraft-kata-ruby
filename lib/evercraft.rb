8d83f09728463ec46142adbc30155a7792c62d86
Guy Royse <guy@guyroyse.com>
2014-01-06 16:21:55 -0500

    can only be good, neutral, and evil

--- lib/evercraft.rb
+++ lib/evercraft.rb
@@ -6,4 +6,9 @@ class Character
     @alignment = :neutral
   end
 
+  def alignment= alignment
+    raise "Invalid alignment" unless [:good, :evil, :neutral].include? alignment
+    @alignment = alignment
+  end
+
 end
--- spec/evercraft_spec.rb
+++ spec/evercraft_spec.rb
@@ -5,17 +5,35 @@ describe Character do
     expect(subject.name).to eq 'Bob'
   end
 
-  it 'no name when created' do
-    expect(subject.name).to be_nil
-  end
-
-  it 'has an alignment' do
+  it 'can be good' do
     subject.alignment = :good
     expect(subject.alignment).to eq :good
   end
 
-  it 'is neutral when created' do
+  it 'can be neutral' do
+    subject.alignment = :neutral
     expect(subject.alignment).to eq :neutral
+  end
+
+  it 'can be evil' do
+    subject.alignment = :evil
+    expect(subject.alignment).to eq :evil
+  end
+
+  it 'cannot be phlegmatic' do
+    expect { subject.alignment = :phlegmatic }.to raise_error
+  end
+
+  context 'when created' do
+
+    it 'has no name' do
+      expect(subject.name).to be_nil
+    end
+
+    it 'is neutral' do
+      expect(subject.alignment).to eq :neutral
+    end
+
   end
 
 end
