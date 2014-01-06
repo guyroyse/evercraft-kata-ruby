8a7eac487c737a028f4c0cc0b52477785e484972
Guy Royse <guy@guyroyse.com>
2014-01-06 16:23:13 -0500

    refactor alignments to constant

--- lib/evercraft.rb
+++ lib/evercraft.rb
@@ -1,13 +1,16 @@
 class Character
 
-  attr_accessor :name, :alignment
+  ALIGNMENTS = [:good, :neutral, :evil]
+
+  attr_accessor :name
+  attr_reader :alignment
 
   def initialize
     @alignment = :neutral
   end
 
   def alignment= alignment
-    raise "Invalid alignment" unless [:good, :evil, :neutral].include? alignment
+    raise "Invalid alignment" unless ALIGNMENTS.include? alignment
     @alignment = alignment
   end
 
